4e963ce3c0a74fe27b69632b468c1c86b46e6fb5
Gilad Reich <giladreichgr@gmail.com>
2017-10-12 18:35:43 +0200

    Adding gui forms structure.

---
+++ src/TeamManager/TeamManager/Forms/ChildForms/UnsignedPlayersForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace TeamManager.Forms.ChildForms
+{
+    partial class UnsignedPlayersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UnsignedPlayersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "UnsignedPlayersForm";
+            this.Text = "UnsignedPlayersForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
