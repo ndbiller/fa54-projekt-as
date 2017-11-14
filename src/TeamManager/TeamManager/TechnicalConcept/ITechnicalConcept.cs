c1b5cfc36596f7569d0c19f1a45b5645e7cd873d
B3ttm3nsch <krueger.rico@web.de>
2017-11-14 18:25:45 +0100

    added Classes for Technical Concept. need more information about dbManager. IDbManager can be deleted.

---
+++ src/TeamManager/TeamManager/TechnicalConcept/ITechnicalConcept.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamManager.Database;
+
+namespace TeamManager.TechnicalConcept
+{
+    public interface ITechnicalConcept
+    {
+        IDbManager ADbManager { get; set; }
+
+        Team ATeam { get; set; }
+        Player APlayer { get; set; }
+        
+        List<String> GetAllTeams();
+        String AddNewTeam(String teamName);
+        String ChangeTeamName(String teamId, String NewTeamName);
+        String RemoveTeam(String teamId);
+
+        List<String> GetAllPlayer();
+        List<String> GetTeamPlayers(String teamId);
+        String AddNewPlayer(String playerName);
+        String ChangePlayerName(String playerId);
+        String RemovePlayer(String playerId);
+        String TransferPlayer(String playerId, String teamId);
+    }
+}
---
+++ src/TeamManager/TeamManager/TechnicalConcept/Team.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamManager.TechnicalConcept
+{
+    public class Team
+    {
+        public String Name
+        {
+            get { return Name; }
+            private set { Name = value; }
+        }
+        public String Id
+        {
+            get { return Id; }
+            private set { Id = value; }
+        }
+        public Team()
+        {
+            this.Name = "New Team";
+        }
+        public Team(String name)
+        {
+            this.Name = name;
+        }
+        public Team(String name, String id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+    }
+}
